7be96fce29a913c566611718e211bf113475ac3c
ZORO-1104 <zhangqi_1104@163.com>
2020-07-31 13:31:23 +0800

    面向对象，属性，被动属性

---
+++ ZqCSharpLearn/OO/A02/OOA02.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZqCSharpLearn.OO.A02
+{
+    class OOA02 : ICodeTest
+    {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Student
+    {
+        private int age;
+
+        public int Age
+        { 
+            get => age; 
+            protected set => age = value;
+        }
+
+        //被动的，外界调用属性时，再根据年龄计算是否可以工作
+        public bool CanWork
+        {
+            get
+            {
+                return this.age >= 16 ? true : false;
+            }
+        }
+    }
+
+    //主动计算的方式
+    class Student2
+    {
+        private int age;
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                age = value;
+                //每当对年龄进行设置数值时，主动调用私有方法，计算是否可以工作的属性
+                CalculateIsCanWork();
+            }
+        }
+
+        private bool canWork;
+
+        public bool CanWork
+        {
+            get { return canWork; }
+        }
+
+        private void CalculateIsCanWork()
+        {
+            if (this.age>=16)
+            {
+                this.canWork = true;
+            }
+            else
+            {
+                this.canWork = false;
+            }
+        }
+    }
+}
