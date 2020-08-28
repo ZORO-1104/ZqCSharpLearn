14f3a4254cabf88e38fbd6bae35f9ee58e8aec89
ZORO-1104 <zhangqi_1104@163.com>
2020-08-28 13:32:42 +0800

    try/catch/finally语句结合return、异常存在时的跳转

---
+++ ZqCSharpLearn/Try/A02/TryA02.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZqCSharpLearn.Try.A02
+{
+    internal class TryA02 : ICodeTest
+    {
+        public void Execute()
+        {
+            Test02();
+            Test03();
+            Test04();
+            Test05();
+            Test06();
+        }
+
+        /// <summary>
+        /// 测试try语句功能
+        /// 在try块中执行return
+        /// </summary>
+        private void Test02()
+        {
+            try
+            {
+                Console.WriteLine("执行...");
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("不会执行...");
+            }
+            finally
+            {
+                Console.WriteLine("执行...");
+            }
+
+            Console.WriteLine($"不会执行...");
+        }
+
+        /// <summary>
+        /// 测试try语句功能
+        /// 在try块中执行return
+        /// </summary>
+        private void Test03()
+        {
+            try
+            {
+                Console.WriteLine("执行...");
+                //return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("不会执行...");
+            }
+            finally
+            {
+                Console.WriteLine("执行...");
+            }
+
+            Console.WriteLine($"执行...");
+        }
+
+        /// <summary>
+        /// 测试try语句功能
+        /// 在try块中执行return
+        /// </summary>
+        private void Test04()
+        {
+            try
+            {
+                Console.WriteLine("执行...");
+                int x = 0;
+                int y = 10 / x;
+
+                Console.WriteLine("不会执行...");
+                //return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("执行...");
+            }
+            finally
+            {
+                Console.WriteLine("执行...");
+            }
+
+            Console.WriteLine($"执行...");
+        }
+
+        /// <summary>
+        /// 测试try语句功能
+        /// 在try块中执行return
+        /// </summary>
+        private void Test05()
+        {
+            try
+            {
+                Console.WriteLine("执行...");
+                int x = 0;
+                int y = 10 / x;
+
+                Console.WriteLine("不会执行...");
+                //return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("执行...");
+                return;
+            }
+            finally
+            {
+                Console.WriteLine("执行...");
+            }
+
+            Console.WriteLine($"不会执行...");
+        }
+
+        /// <summary>
+        /// 测试try语句功能
+        /// 在try块中执行return
+        /// </summary>
+        private void Test06()
+        {
+            try
+            {
+                Console.WriteLine("执行...");
+                int x = 0;
+                int y = 10 / x;
+
+                Console.WriteLine("执行...");
+                //return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("执行...");
+            }
+            finally
+            {
+                Console.WriteLine("执行...");
+                //return;这会编译报错（控制不能离开finally字句主体）
+            }
+
+            Console.WriteLine($"执行...");
+        }
+    }
+}
