388da4305d667d06c820bc3abb8f6290c16d3a74
ZORO-1104 <zhangqi_1104@163.com>
2020-09-03 16:24:35 +0800

    Task.Delay异步延时

---
+++ ZqCSharpLearn/MultiThread/B05/MultiThreadB05.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ZqCSharpLearn.MultiThread.B05
+{
+    internal class MultiThreadB05 : ICodeTest
+    {
+        public void Execute()
+        {
+            Test01();
+            Test02();
+        }
+
+        /// <summary>
+        /// 利用Task.Delay进行异步延时判断
+        /// </summary>
+        private void Test02()
+        {
+            Task<int> txxxx = Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                return 100;
+            });
+
+            Task.Delay(5000).ContinueWith(t =>
+            {
+                if (txxxx.IsCompleted == true)
+                {
+                    Console.WriteLine($"task completed.");
+                }
+                else
+                {
+                    Console.WriteLine($"task not completed.");
+                }
+            });
+
+            Console.WriteLine($"test02 is end.");
+        }
+
+        /// <summary>
+        /// Task.Delay 是一个异步延时方法
+        /// （Thread.Sleep是同步延时）
+        /// </summary>
+        private void Test01()
+        {
+            Console.WriteLine($"Time={DateTime.Now}，Test01 start running...");
+
+            Task.Delay(5000).ContinueWith(t =>
+            {
+                Console.WriteLine($"Time={DateTime.Now}，delay running...");
+                Thread.Sleep(5000);
+                Console.WriteLine($"Time={DateTime.Now}，delay end...");
+            });
+
+            Console.WriteLine($"Time={DateTime.Now}，Test01 end...");
+        }
+    }
+}
