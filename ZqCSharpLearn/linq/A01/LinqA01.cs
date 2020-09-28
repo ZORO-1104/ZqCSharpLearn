38bf74ae910184513280c8e7548580a7f9a07917
ZORO-1104 <zhangqi_1104@163.com>
2020-09-28 14:37:03 +0800

    延时处理测试

---
+++ ZqCSharpLearn/linq/A01/LinqA01.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZqCSharpLearn.Linq.A01
+{
+    internal class LinqA01 : ICodeTest
+    {
+        public void Execute()
+        {
+            Test01();
+        }
+
+        private void Test01()
+        {
+            List<string> strArray = new List<string>() { "golbal", "future", "confidence", "world" };
+
+            IEnumerable<string> arr2 = strArray.Where(n => n.Contains("e"));
+            foreach (var item in arr2)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("-------");
+
+            strArray.Clear();
+            foreach (var item in arr2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
