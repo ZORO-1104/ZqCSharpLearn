6a748759a2498ccb2128cbc706d4352465e2abab
ZORO-1104 <zhangqi_1104@163.com>
2020-11-06 14:12:49 +0800

    接口的显示实现与调用

---
+++ ZqCSharpLearn/OO/A10/OOA10.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZqCSharpLearn.OO.A10
+{
+    internal class OOA10 : ICodeTest
+    {
+        public void Execute()
+        {
+            Test01();
+        }
+
+        private void Test01()
+        {
+            ClassA c1 = new ClassA();
+            //c1.Fun();//无法调用，因为ClassA的接口都显示实现了，必须转换为相应的接口才可调用
+            ((IA)c1).Fun();//IA.Fun...
+            ((IB)c1).Fun();//Ib.Fun...
+        }
+    }
+
+    internal interface IA
+    {
+        void Fun();
+    }
+
+    internal interface IB
+    {
+        int Fun();
+    }
+
+    internal class ClassA : IA, IB
+    {
+        void IA.Fun()
+        {
+            Console.WriteLine("IA.Fun...");
+        }
+
+        int IB.Fun()
+        {
+            Console.WriteLine("IB.Fun...");
+            return 0;
+        }
+    }
+}
